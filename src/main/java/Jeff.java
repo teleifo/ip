eb85eb3a5f062e9d498bc9addb2c854ea2af66ba
teleifo <tantrevor30@gmail.com>
2026-01-23 04:32:28 +0800

    Add list functionality

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,32 +1,54 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jeff {
+    private static final ArrayList<String> list = new ArrayList<String>();
+
+    private static String formatReply(String message) {
+        return String.format("""
+                ____________________________________________________________
+                %s
+                ____________________________________________________________""", message);
+    }
+
+    private static String handleCommand(String command) {
+        if (command.equals("list")) {
+            if (list.isEmpty()) {
+                return formatReply("List is currently empty!");
+            } else {
+                StringBuilder temp = new StringBuilder();
+                for (int i = 1; i <= list.size(); i++) {
+                    temp.append(i).append(". ").append(list.get(i-1));
+                    if (i != list.size()) temp.append("\n");
+                }
+                return formatReply(temp.toString());
+            }
+        } else if (command.equals("bye")) {
+            return formatReply("Bye. Hope to see you again soon!");
+        } else {
+            list.add(command);
+            return formatReply(String.format("Added: %s", command));
+        }
+    }
+
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
 
-        String greeting = "____________________________________________________________\n"
-                + "Hello! I'm Jeff\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________";
+        String greeting = """
+                ____________________________________________________________
+                Hello! I'm Jeff
+                What can I do for you?
+                ____________________________________________________________""";
         System.out.println(greeting);
 
         boolean loop = true;
         while (loop) {
             String command = sc.nextLine();
-            String reply = "";
 
-            if (command.equals("bye")) {
-                loop = false;
-                reply = "____________________________________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "____________________________________________________________";
-            } else {
-                reply = "____________________________________________________________\n"
-                        + command + "\n"
-                        + "____________________________________________________________";
-            }
-
+            String reply = handleCommand(command);
             System.out.println(reply);
+
+            if (command.equals("bye")) loop = false;
         }
 
         sc.close();
