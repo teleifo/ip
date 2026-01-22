288c948368f326c0496fd350aa33e21089f7cc68
teleifo <tantrevor30@gmail.com>
2026-01-23 03:46:54 +0800

    Add echo functionality

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Jeff {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+
         String greeting = "____________________________________________________________\n"
                 + "Hello! I'm Jeff\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
         System.out.println(greeting);
+
+        boolean loop = true;
+        while (loop) {
+            String command = sc.nextLine();
+            String reply = "";
+
+            if (command.equals("bye")) {
+                loop = false;
+                reply = "____________________________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________";
+            } else {
+                reply = "____________________________________________________________\n"
+                        + command + "\n"
+                        + "____________________________________________________________";
+            }
+
+            System.out.println(reply);
+        }
+
+        sc.close();
     }
 }
