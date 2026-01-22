b1c990dad4db8a70904133cde33b36ef8eec9dd0
teleifo <tantrevor30@gmail.com>
2026-01-23 02:40:16 +0800

    Add greeting

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,11 @@
+public class Jeff {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n"
+                + "Hello! I'm Jeff\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(greeting);
+    }
+}
